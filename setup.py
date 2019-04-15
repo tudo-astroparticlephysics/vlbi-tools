daad7ad901c3a8e7af3b115b93d4b8078c7d9e2b
Kevin Schmidt <kevin3.schmidt@udo.edu>
2019-04-15 16:59:26 +0200

    adding setup and init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='vlbi-tools',
+      version='0.0.1',
+      description='Tools for analyzing and presenting VLBI data',
+      url='https://github.com/tudo-astroparticlephysics/vlbi-tools',
+      author='Kevin Schmidt',
+      author_email='kevin3.schmidt@tu-dortmund.de',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[],
+      classifiers=[
+          'Programming Language :: Python :: 3.6',
+      zip_safe=False,
+     )
