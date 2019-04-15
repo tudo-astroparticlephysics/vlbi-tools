d01ff9b95d5bd54e64d62cb373ccdb8b5d70e149
Kevin Schmidt <kevin3.schmidt@udo.edu>
2019-04-15 17:27:07 +0200

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import setup, find_packages
 
-setup(name='vlbi-tools',
-      version='0.0.1',
-      description='Tools for analyzing and presenting VLBI data',
-      url='https://github.com/tudo-astroparticlephysics/vlbi-tools',
-      author='Kevin Schmidt',
-      author_email='kevin3.schmidt@tu-dortmund.de',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=[],
-      classifiers=[
-          'Programming Language :: Python :: 3.6',
-      zip_safe=False,
-     )
+setup(
+    name='vlbi-tools',
+    version='0.0.1',
+    description='Tools for analyzing and presenting VLBI data',
+    url='https://github.com/tudo-astroparticlephysics/vlbi-tools',
+    author='Kevin Schmidt',
+    author_email='kevin3.schmidt@tu-dortmund.de',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+         'Development Status :: 2 - Pre-Alpha',
+         'Intended Audience :: Science/Research',
+         'License :: OSI Approved :: MIT License',
+         'Natural Language :: English',
+         'Operating System :: OS Independent',
+         'Programming Language :: Python',
+         'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3 :: Only',
+         'Topic :: Scientific/Engineering :: Astronomy',
+         'Topic :: Scientific/Engineering :: Physics',
+    ],
+    zip_safe=False,
+)
